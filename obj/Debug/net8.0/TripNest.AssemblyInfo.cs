a17def442583689371095fd686f4768fba9090b7
Manoj Shrestha <stha8841@gmail.com>
2025-09-07 19:11:05 +0545

    final

--- obj/Debug/net8.0/TripNest.AssemblyInfo.cs
+++ obj/Debug/net8.0/TripNest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TripNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b383551c5a03084c8eaf75f2cd811c1482149fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5905b1ed1d584c43c76366296b56480c03740b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TripNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TripNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
